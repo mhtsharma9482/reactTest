e5e9d9e05991e3c10a98214c039ba662c376880d
mhtsharma9482 <mohit.sharma@causecode.com>
2017-07-13 17:25:54 +0530

    login
    . added axios and lodash
    . added onsubmit event on form
    . added ref for getting data from text fiels
    . added async request to fake json server

--- app/components/Login/index.tsx
+++ app/components/Login/index.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import axios from 'axios';
+import {isEmpty} from 'lodash';
 const styles = require('./styles.module.css');
 interface ILoginProps extends React.ClassAttributes<Login> {
   abc ?: string;
@@ -7,12 +9,24 @@ interface ILoginProps extends React.ClassAttributes<Login> {
  * Login
  */
 class Login extends React.Component<{}, {}> {
+  /*private login() {
+    // debugger;
+    console.log("login");
+    axios.get('http://localhost:3000/user?username=Mohit').then(function (response) {
+      console.log(response.data);
+    });
+  }*/
   public render() {
     return (
-      <form className={styles.loginForm}>
-        <div>Name <input type="text" name="username" /></div>
-        <div>Password <input type="password" name="password" /></div>
-        <input type="submit" />
+      <form className={styles.loginForm} onSubmit={(): void => {
+            axios.get('http://localhost:3000/user?username='+this.refs.userNameRef.value+'&password='+this.refs.passwordRef.value)
+            .then(function (response) {
+              console.log(response.data);
+            });
+          }}>
+        <div>Name <input type="text" ref= "userNameRef" name="username" /></div>
+        <div>Password <input type="password" ref="passwordRef" name="password" /></div>
+        <button type="submit">submit</button>
       </form>
     );
   }
